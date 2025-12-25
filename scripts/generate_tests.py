32af0fa0972bbe83914053372c208d293a7406ed
Albert-Iulian Romaniuc <albert.romaniuc@stud.acs.upb.ro>
2025-12-25 17:28:50 +0200

    added graph test generator and tests

---
+++ scripts/generate_tests.py
@@ -0,0 +1,76 @@
+import random
+import os
+
+NMAX = 20
+
+def generate_graph(n, density):
+    """
+    Generates an undirected graph with n vertices and given density.
+    Density is a value between 0 and 1.
+    """
+
+    max_edges = n * (n - 1) // 2
+    target_edges = int(density * max_edges)
+
+    edges = []
+    used = set()
+
+    while len(edges) < target_edges:
+        u = random.randint(0, n - 1)
+        v = random.randint(0, n - 1)
+
+        if u == v:
+            continue
+
+        if u > v:
+            aux = u
+            u = v
+            v = aux
+
+        if (u, v) in used:
+            continue
+
+        used.add((u, v))
+        edges.append((u, v))
+
+    return edges
+
+
+def write_test(filename, n, edges):
+    """
+    Writes the graph to a file using the required input format.
+    """
+
+    with open(filename, "w") as f:
+        f.write(str(n) + " " + str(len(edges)) + "\n")
+
+        for edge in edges:
+            u = edge[0]
+            v = edge[1]
+            f.write(str(u) + " " + str(v) + "\n")
+
+
+def main():
+    random.seed(42)
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    tests_dir = os.path.join(base_dir, "..", "tests")
+
+    if not os.path.exists(tests_dir):
+        os.mkdir(tests_dir)
+    test_id = 1
+
+    sizes = [4, 8, 10, 12, 15, 18, 20]
+    densities = [0.2, 0.4, 0.6, 0.8]
+
+    for n in sizes:
+        for density in densities:
+            edges = generate_graph(n, density)
+
+            filename = os.path.join(tests_dir, "test_" + str(test_id).zfill(2) + ".in")
+            write_test(filename, n, edges)
+            test_id += 1
+
+
+if __name__ == "__main__":
+    main()
